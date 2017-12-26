81bfd405ddea57655176a975985d35ca7cba7ece
Pranav Burugula <pranav.burugula@gmail.com>
2017-12-25 20:28:43 -0500

    New Version

--- src/ParseSeizures.java
+++ src/ParseSeizures.java
@@ -9,17 +9,12 @@ public class ParseSeizures {
     public static void main(String[] args)
     {
         JSONObject rootObj = new JSONObject();
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("{\n");
 
         String root = "D:\\Documents\\database_mit\\";
         for(int i=1;i<24;i++)
         {
             String suffix = "chb" + (i<10 ? ("0"+i) : i);
-            stringBuilder.append(String.format("'%s' : [\n", suffix));
             String path = root + suffix + "\\" + suffix + "-summary.txt";
-            System.out.println(path);
-
             JSONArray jsonSeizuresArr = new JSONArray();
             try
             {
@@ -40,8 +35,6 @@ public class ParseSeizures {
                         String startTime = bufferedReader.readLine().substring(17);
                         String endTime = bufferedReader.readLine().substring(15);
                         int numSeizures = Integer.parseInt(bufferedReader.readLine().substring(28));
-
-                        System.out.println(fileName + "\n" + startTime + "\n" + endTime + "\n" + numSeizures);
 
                         if (numSeizures > 0)
                         {
@@ -70,34 +63,17 @@ public class ParseSeizures {
                             jsonSeizureObj.put("SeizureStartTimes", jsonSeizureStartTimes);
                             jsonSeizureObj.put("SeizureEndTimes", jsonSeizureEndTimes);
                             jsonSeizuresArr.add(jsonSeizureObj);
-                            String seizureStartString = "[" + seizureStart.get(0);
-                            String seizureEndString = "[" + seizureEnd.get(0);
-                            for (int j=1;j<seizureStart.size();j++)
-                            {
-                                seizureStartString += "," + seizureStart.get(j);
-                                seizureEndString += "," + seizureEnd.get(j);
-                            }
-
-                            seizureStartString +="]";
-                            seizureEndString+="]";
-
-                            String lineToAppend = String.format("{'FileName' : '%s', 'StartTime' : '%s', 'EndTime' : '%s', 'SeizureStartTime' : %s, 'SeizureEndTime' : %s},\n",
-                                    fileName, startTime, endTime, seizureStartString, seizureEndString);
-                            stringBuilder.append(lineToAppend);
                         }
                     }
                     line = bufferedReader.readLine();
                 }
-                Scanner in = new Scanner(System.in);
             } catch (IOException e)
             {
                 e.printStackTrace();
             }
-            stringBuilder.append("],\n");
             rootObj.put(suffix, jsonSeizuresArr);
         }
-        stringBuilder.append("}");
-        System.out.println(stringBuilder);
+
         File file = new File("D:\\Documents\\python_workspace\\eegAnalysis\\Configuration\\seizures.json");
 
         try
